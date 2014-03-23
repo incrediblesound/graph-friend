7009c4f7306b39a90f73b0bd9e7e6415939fb570
James H. Edwards <james.howe.edwards@gmail.com>
2014-03-23 13:57:22 +0800

    sort users with labels

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,8 @@ exports.index = function(req, res){
   res.render('index');
 };
 
-exports.register = function(req, res) {
-  if(req.body.status === 'Lord') {
-    var query = 'CREATE (n: Lord {name: "'+req.body.name+'", password: "'+req.body.password+'", job: "'+req.body.job+'"})'
-  } else {
-    var query = 'CREATE (n: Minion {name: "'+req.body.name+'", password: "'+req.body.password+'", job: "'+req.body.job+'"})'
-  }
-  db.query(query, function (err) {
+exports.register = function(req, res) { 
+  db.query('CREATE (n: '+req.body.status+' {name: "'+req.body.name+'", password: "'+req.body.password+'", job: "'+req.body.job+'"})', function (err) {
     req.session.data = {name: req.body.name, password: req.body.password};
     res.redirect('/login');
 	})
@@ -24,33 +19,40 @@ exports.register = function(req, res) {
 exports.login = function(req, res) {
   if(req.body.name && req.body.password) {
     var loginData = {name: req.body.name, password: req.body.password};
-  } 
-  else if (req.session.data) {
+  } else if (req.session.data) {
     var loginData = req.session.data;
-  } else {
-    res.redirect('/');
-  }
+  } else { res.redirect('/'); }
+
   var query = [
   'MATCH n',
   'WHERE n.name = "'+loginData.name+'"',
   'AND n.password = "'+loginData.password+'"',
-  'RETURN n'
+  'RETURN n, labels(n)'
   ].join('\n');
+
   db.query(query, function (err, user) {
     if(err) {
       console.log(err);
       res.redirect('/');
     } else {
     req.session.user = user[0].n;
+    req.session.labels = user[0]['labels(n)'][0];
     req.session.userId = user[0].n.id;
-    db.query('MATCH n\nWHERE n.job = "'+req.session.user.data.job+'"\nRETURN n', function ( err, sameJob) {
+    if(req.session.labels === 'Minion') {
+        var complement = 'Lord';
+      } else {
+        var complement = 'Minion';
+      }
+    db.query('MATCH (n:'+complement+')\nWHERE n.job = "'+req.session.user.data.job+'"\nRETURN n', function ( err, sameJob) {
     if(err) {
       console.log(err);
       res.redirect('/');
     } else {
+      console.log(req.session.labels);
         res.render('home', {
           user: req.session.user,
-          others: sameJob
+          others: sameJob,
+          otherstatus: complement
         })
       }
     })
